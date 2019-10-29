cf34b4f08693e73beaaa664df23449657729e562
sjconst <stephanie.c.lake@gmail.com>
2019-10-29 09:18:33 -0700

    adding callbacks to controller

--- controllers/reading_controller.js
+++ controllers/reading_controller.js
@@ -13,7 +13,14 @@ router.put("/api/books/:id", (req, res) => {
     let condition = `bookID = ${req.params.id}`;    
     let col = `is_read`;
     let val = req.body.read;
-    readingModel.updateOne(col, val, condition);
+    readingModel.updateOne(col, val, condition, result => {
+        if(result.changedRows === 0){
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+        } else{
+            res.status(200).end();
+        }
+    });
 });
 router.post("/api/books", (req, res) => {
     readingModel.insertOne([
@@ -21,13 +28,22 @@ router.post("/api/books", (req, res) => {
     ], [
         `"${req.body.title}"`, `"${req.body.firstName}"`, `"${req.body.lastName}"`
     ], result => {
-        res.json({id: result.bookID});    
+        res.json({
+            id: result.bookID
+        });    
     })
 });
 router.delete("/api/books/:id", (req, res) => {
     let col = `bookID`;
     let val = req.params.id;  
-    readingModel.delete(col, val);
+    readingModel.delete(col, val, result => {
+        if(result.affectRows === 0){
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();    
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 //Export
 module.exports = router;
