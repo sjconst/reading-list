7b7813eaf7a48ac73cbae4c52e09c381ffbd79f6
sjconst <stephanie.c.lake@gmail.com>
2019-10-29 11:59:51 -0700

    added connection option to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require("dotenv").config();
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "library_db"
-});
+const connection;
+//Connection options for Heroku vs localhost
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "library_db"
+  });
+} 
 // Make connection.
 connection.connect(err => {
   if (err) {
